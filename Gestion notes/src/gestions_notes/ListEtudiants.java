71c0234eabfea11d5a9b33943d83046819147bf3
saad2 <saad2@Saad-PC.mshome.net>
2022-04-15 15:43:35 +0000

    ListeEtudiants

--- Gestion notes/src/gestions_notes/ListEtudiants.java
+++ Gestion notes/src/gestions_notes/ListEtudiants.java
@@ -3,5 +3,13 @@ package gestions_notes;
 import java.util.ArrayList;
 
 public class ListEtudiants extends ArrayList<etudiant>{
+	public boolean add(etudiant e) {
+		if(this.indexOf(e)>0) {
+			int i=indexOf(e);
+			super.set(i, e);
+			return true;
+		}
+		return super.add(e);
+	}
 
 }
