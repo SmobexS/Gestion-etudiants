64952cdfdcef8405af103500bbc0a168b15e71df
saad2 <saad2@Saad-PC.mshome.net>
2022-04-15 16:33:16 +0000

    Main

--- Gestion notes/src/gestions_notes/main.java
+++ Gestion notes/src/gestions_notes/main.java
@@ -1,5 +1,7 @@
 package gestions_notes;
 
 public class main {
-	etudiant ()
+	etudiant e1 = new etudiant(1,"hamid");
+	etudiant e2 = new etudiant(2,"driss");
+	etudiant e3 = new etudiant(3,"youssef");
 }
